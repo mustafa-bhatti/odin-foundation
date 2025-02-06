06e14fae1dcaa2faea2b0f161c9bd8d31066ece3
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-06 23:35:08 +0500

    String Concatenation

--- js/script.js
+++ js/script.js
@@ -64,3 +64,11 @@ function numberChecker() {
        return false;
    }
 }
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+
+console.log(greeting);
