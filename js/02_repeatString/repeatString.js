cae1b954678258b03e5cc1e45740841b95ec6875
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-12 23:44:11 +0500

    add 02 excercise js RepeatString

---
+++ js/02_repeatString/repeatString.js
@@ -0,0 +1,13 @@
+const repeatString = function(str1,num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let repeatString = "";
+    for (let i = 0;i<num;i++){
+        repeatString += str1
+    }
+    return repeatString;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
