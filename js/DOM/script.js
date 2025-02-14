2811e04f521141aaf9e0d1b32ebc45172c6ae588
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-15 00:19:36 +0500

    add a red text

--- js/DOM/script.js
+++ js/DOM/script.js
@@ -6,3 +6,9 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+const red = document.createElement("p");
+red.classList.add("red");
+red.style.color = "red";
+red.textContent = "Hey I'm Red";
+container.appendChild(red);
