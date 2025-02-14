ef666f64ef2d41b6fa7417ddb54ba272553ccd6f
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-15 00:28:21 +0500

    Add a div with two children h1 and p

--- js/DOM/script.js
+++ js/DOM/script.js
@@ -8,13 +8,23 @@ content.textContent = "This is the glorious text-content!";
 container.appendChild(content);
 
 const red = document.createElement("p");
-red.classList.add("red");
 red.style.color = "red";
 red.textContent = "Hey I'm Red";
 container.appendChild(red);
 
 const blue = document.createElement("h3");
-blue.classList.add("blue");
 blue.style.color = "blue";
 blue.textContent = "Hey I'm blue";
 container.appendChild(blue);
+
+const div = document.createElement("div");
+div.style.cssText = "background-color: pink; border:1px solid black;";
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+div.appendChild(h1);
+
+const p = document.createElement("p");  
+p.textContent = "Me too";
+div.appendChild(p);
+
+container.appendChild(div);
