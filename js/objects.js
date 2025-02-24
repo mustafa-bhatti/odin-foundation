6c1ddef852a6e8dd362b5dc16d8fe2dfbbadfe58
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-24 21:56:14 +0500

    Add camelize function

--- js/objects.js
+++ js/objects.js
@@ -6,3 +6,25 @@ Then it will sum all those even numbers. */
 let arr = [1,2,3,4,5,6];
 let sumOfTripleEvens = arr.filter(x => x % 2 === 0).map(x => x * 3).reduce((total,current) => total + current);
 console.log(sumOfTripleEvens);
+
+function camelize(str) {
+    str = str.toLowerCase();
+    let arr = str.split("-");
+    let firstElement = arr.shift();
+    let restArr = arr.map(x =>x[0].toUpperCase()+x.slice(1)).join("");
+    console.log(firstElement+restArr);
+
+// }
+//Solution from the internet
+// function camelize(str) {
+//     return str
+//       .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+//       .map(
+//         // capitalizes first letters of all array items except the first one
+//         // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+//         (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+//       )
+//       .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+//   }
+
+camelize("backGRound-color-yess");
