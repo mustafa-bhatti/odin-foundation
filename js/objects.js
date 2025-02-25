7a1b5b43c1a87795f583e317aa801d564f3203b7
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-26 01:33:21 +0500

    add groupById func

--- js/objects.js
+++ js/objects.js
@@ -5,7 +5,7 @@ Then it will sum all those even numbers. */
 
 let arr = [1,2,3,4,5,6];
 let sumOfTripleEvens = arr.filter(x => x % 2 === 0).map(x => x * 3).reduce((total,current) => total + current);
-console.log(sumOfTripleEvens);
+// console.log(sumOfTripleEvens);
 
 function camelize(str) {
     str = str.toLowerCase();
@@ -27,7 +27,7 @@ function camelize(str) {
 //       .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
 //   }
 
-camelize("backGRound-color-yess");
+// camelize("backGRound-color-yess");
 arr = [5,3,8,1]
 function filterRange(arr,start,end) {
     return arr.filter(item => item >= start && item <=end)
@@ -43,5 +43,51 @@ function copySorted(arr) {
 }
 
 let sorted = copySorted(arr);
-console.log(sorted);
-console.log(arr);
+// console.log(sorted);
+// console.log(arr);
+
+let john = { name: "John", surname: "Smith", id: 1 ,age:25};
+let pete = { name: "Pete", surname: "Hunt", id: 2,age:30 };
+let mary = { name: "Mary", surname: "Key", id: 3 ,age:28};
+
+let users = [ john, pete, mary ];
+
+let names = users.map(x => x.name);
+console.log(names);
+
+let userMapped = users.map(x => ({fullname:`${x.name} ${x.surname}`,id:x.id}));
+console.log(userMapped);
+
+function sortByAge(arr) {
+    arr.sort((a,b) => a.age - b.age)
+}
+
+sortByAge(users);
+console.log(users);
+
+
+function unique(arr) {
+    /* your code */
+    let newArr = [];
+    for (key in arr)    {
+        if (!newArr.includes(arr[key])) {
+            newArr.push(arr[key]);
+        }
+    }
+    return newArr;
+  }
+  
+  let strings = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+  
+console.log(unique(strings));
+
+function groupById(array) {
+    return array.reduce((obj, value) => {
+      obj[value.id] = value;
+      return obj;
+    }, {})
+  }
+
+console.log(groupById(users));
